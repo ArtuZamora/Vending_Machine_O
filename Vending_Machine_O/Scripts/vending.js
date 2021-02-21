b2d1bda1f3f312a383e29ead40d3063fed560ba7
Rafael Arturo Zamora Aguilar <r2zamora11@gmail.com>
2021-02-21 17:00:19 -0600

    Buying process finished.

--- Vending_Machine_O/Scripts/vending.js
+++ Vending_Machine_O/Scripts/vending.js
@@ -1,11 +1,13 @@
 ﻿const times = ['10000', '5000', '7000', '5000', '15000', '2000', '10000', '15000'];
-var divProd = document.getElementsByClassName("view_main");
+const images = ['crepas.png', 'hamburguesa.png', 'hotdog.png', 'papas_fritas.png', 'pizza.png', 'soda.png', 'sorbete.png', 'tacos.png']
 var btnProd = document.getElementById("products_option");
 var btnPcss = document.getElementById("process_option");
 var btnFnsh = document.getElementById("finished_option");
-var buy_btns = document.getElementsByClassName("btn");
+var image = document.getElementById("foodImg");
 var buyAlert_overlay = document.getElementById("shim");
 var buyAlert_message = document.getElementById("msgbx");
+var buy_btns = document.getElementsByClassName("btn");
+var divProd = document.getElementsByClassName("view_main");
 var finished = [];
 var proccess = [];
 var product;
@@ -31,6 +33,15 @@ function ShowProcess() {
     divProd[0].style.display = "none";
     divProd[1].style.display = "block";
     divProd[2].style.display = "none";
+    if (proccess.length === 0) {
+        document.getElementById("pc_p_1").style.display = "block";
+        document.getElementById("pc_p_2").style.display = "none";
+        image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png";
+    }
+    else {
+        image.src = "Content/Images/" + images[proccess[0] - 1];
+    }
+    console.log(proccess);
 }
 function ShowFinished() {
     btnFnsh.classList.add("active");
@@ -47,9 +58,13 @@ function BuyProduct(id) {
 }
 
 function Accept() {
+    document.getElementById("pc_p_1").style.display = "none";
+    document.getElementById("pc_p_2").style.display = "block";
     HideMessage();
     proccess.push(product);
-    console.log(proccess);
+    if (proccess.length === 1) {
+        setTimeout(ProcessFood, times[product - 1]);
+    }
 }
 
 function Decline() {
@@ -60,4 +75,14 @@ function HideMessage() {
     setTimeout(function () {
         buyAlert_message.style.display = buyAlert_overlay.style.display = "none";
     }, 50);
+}
+
+function ProcessFood() {
+    finished.push(proccess[0]);
+    proccess.shift();
+    toastr.info('El item ha sido entregado!')
+    image.src = "Content/Images/" + images[proccess[0] - 1];
+    if (proccess.length !== 0) {
+        setTimeout(ProcessFood, times[proccess[0] - 1]);
+    }
 }
